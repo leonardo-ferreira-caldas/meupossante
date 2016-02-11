ee86f8baa228d10705c6dfd58365ad1fb43bb8f8
Leonardo Caldas <leonardo.ferreira.caldas@gmail.com>
2016-02-11 20:13:55 -0200

    changes

--- teste.js
+++ teste.js
@@ -1,5 +1,29 @@
-var db = require('./schema');
+var http = require('http');
+var c = Date.now();
+var finished = 0;
 
-for (var i = 200000; i < 400000; i++) {
-    db.crawler.collection.insert({url: "/compras/carros/" + i});
+for (var i = 0; i < 1000; i++) {
+
+    http.get({
+        host: 'google.com.br',
+        path: '/',
+        agent: false,
+        port: 80
+    }, function (response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function (d) {
+            body += d;
+        });
+        response.on('end', function () {
+
+            finished++;
+
+            if (finished == 1000) {
+                console.log(Date.now() - c);
+            }
+
+        });
+    });
+
 }
